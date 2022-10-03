85c79f8d54b01d5a87bf1ee7d1d13b5f358e5c39
Antrâxx <h.julien62@gmail.com>
2022-10-03 14:53:05 +0200

    Base blade, Home route, controller and view

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+@section('title')
+    Accueil - @parent
+@endsection
+
+@section('content')
+
+<h1 class="w-2/6 mx-auto text-center text-6xl border-4  rounded-xl my-6 p-6">Laravel - React</h1>
+
+@endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,6 +11,38 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    
+    <header class="flex flex-row justify-between p-4">
+        <div>
+            <ul class="flex flex-row gap-3">
+                <li>
+                    <p>{{ config('app.name') }}</p>
+                </li>
+                <li>
+                    <a href="{{ route('home') }}">Accueil</a>
+                </li>
+                <li>
+                    <a href="#">Jeux</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <ul class="flex flex-row gap-3">
+                <li>
+                    <a href="#">Se connecter</a>
+                </li>
+                <li>
+                    <a href="#">S'inscrire</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+
+    @yield('content')
+
+    <footer class="text-center text-3xl p-6">
+        Copyright &copy {{ now()->year }} - {{ config('app.name') }}
+    </footer>
+
 </body>
 </html>
